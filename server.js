1e3d15c5c91f442790673ab946805d6b2b8f8610
PyGodoy <godoyvitorio99@gmail.com>
2026-03-21 19:57:28 -0300

    vercel

--- server.js
+++ server.js
@@ -2,8 +2,11 @@ require('dotenv').config();
 
 const app = require('./src/app');
 
-const PORT = process.env.PORT || 7000;
-
-app.listen(PORT, () => {
+if (process.env.NODE_ENV !== 'production') {
+  const PORT = process.env.PORT || 7000;
+  app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
-});
+  });
+}
+
+module.exports = app;
